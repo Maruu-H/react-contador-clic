b28c72f3d822b819d595495f7b34de63f56c539b
Maruu-H <mauraruminothernandez@gmail.com>
2022-11-09 14:49:24 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import Boton from './componentes/Boton.js';
+import Contador from './componentes/Contador.js';
+import FreeCodeCampLogo from '../src/imagenes/FreeCodeCampLogo.png'
+import { useState } from 'react'
+
+
+function App() {
+
+  const [numClics , setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+
+  }
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+
+  }
+
+  return (
+    <div className="App">
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          className='freecodecamp-logo'
+          src={FreeCodeCampLogo}
+          alt='Logo de freeCodeCamp' />
+      </div>
+      {/* Botones de contador */}
+
+      <div className='contenedor-principal'>
+        <Contador numClics={numClics} />
+        <Boton
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={manejarClic} />
+        <Boton 
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}/>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
